52cc46cc06d33ab8ec45d0f7e17e77145a6bab87
VaibhavRajpoot <vaibhavrajpoot46@gmail.com>
2022-08-16 02:42:34 +0530

    rviz not working

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,3 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("mastering_ros_robot_description_pkg")
